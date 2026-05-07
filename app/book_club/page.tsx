b6ae36404f4157e27ec558a3a61f128525adc23a
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2026-05-06 20:04:50 -0400

    Improve styling

--- app/book_club/page.tsx
+++ app/book_club/page.tsx
@@ -36,7 +36,7 @@ export default async function bookClub() {
   const currentBook = data ? (data[0] ?? null) : null
 
   return (
-    <main className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <Image
         src={background}
         className="object-cover -z-10"
@@ -46,43 +46,49 @@ export default async function bookClub() {
         alt=""
       />
       {session && (
-        <div>
-          <h1 className="text-center sm:text-left text-4xl font-bold text-gray-900 dark:text-gray-200">
+        <div className="mx-auto max-w-3xl">
+          <h1 className="mb-6 text-center sm:text-left text-4xl font-bold text-gray-900 dark:text-gray-200">
             {currentBook && currentBook.theme_title}
           </h1>
-          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            <div className="mt-8">
-              {currentBook && (
-                <Image
-                  src={currentBook && currentBook.cover_art}
-                  alt="Book cover art"
-                  width={200}
-                  height={400}
-                  loading="eager"
-                  className="w-full sm:w-auto"
-                />
-              )}
-            </div>
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
-                {currentBook && currentBook.title}
-              </h2>
-              <h3 className="mt-3 text-2xl text-gray-700 dark:text-gray-500">
-                {currentBook && currentBook.author}
-              </h3>
-            </div>
-          </div>
-          <div className="text-center sm:text-left mt-8">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
-              Schedule
-            </h2>
-            <div>
-              <ol>
-                <li>Week 1: Ch. 1 - 9</li>
-                <li>Week 2: Ch. 10 - 17</li>
-                <li>Week 3: Ch. 18 - 26</li>
-                <li>Week 4: Ch. 27 - 37</li>
-              </ol>
+          <div className="overflow-hidden rounded-lg bg-white shadow-sm dark:bg-gray-800/50 dark:shadow-none dark:outline dark:-outline-offset-1 dark:outline-white/10">
+            <div className="px-4 py-5 sm:p-6">
+              <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                <div className="">
+                  {currentBook && (
+                    <Image
+                      src={currentBook && currentBook.cover_art}
+                      alt="Book cover art"
+                      width={200}
+                      height={400}
+                      loading="eager"
+                      className="w-full sm:w-auto"
+                    />
+                  )}
+                </div>
+                <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0 flex flex-col">
+                  <div>
+                    <h2 className="text-center sm:text-left text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
+                      {currentBook && currentBook.title}
+                    </h2>
+                    <h3 className="text-center sm:text-left mt-3 text-2xl text-gray-700 dark:text-gray-500">
+                      {currentBook && currentBook.author}
+                    </h3>
+                  </div>
+                  <div className="text-center sm:text-left mt-8 sm:mt-12">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
+                      Schedule
+                    </h2>
+                    <div>
+                      <ol>
+                        <li>Week 1: Ch. 1 - 9</li>
+                        <li>Week 2: Ch. 10 - 17</li>
+                        <li>Week 3: Ch. 18 - 26</li>
+                        <li>Week 4: Ch. 27 - 37</li>
+                      </ol>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
