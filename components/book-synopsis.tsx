9717ea1d09474768a373ade9fc764f90501c9366
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2026-05-28 10:29:25 -0400

    Add collapsible book synopsis

---
+++ components/book-synopsis.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { ReactNode, useState } from "react"
+
+export default function BookSynopsis({ children }: { children: ReactNode }) {
+  const [open, setOpen] = useState(false)
+  return (
+    <div className="mt-2" onClick={() => setOpen(!open)}>
+      <p
+        className={`text-sm text-gray-200 ${open ? "h-fit" : "overflow-hidden h-10 mask-b-from-black"}`}
+      >
+        {children}
+      </p>
+    </div>
+  )
+}
