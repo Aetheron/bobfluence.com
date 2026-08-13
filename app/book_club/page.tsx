35a406fa4894cd29419f36e2623ef42419d42395
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2026-08-13 14:18:50 -0400

    Try to make live updates less glitchy

--- app/book_club/page.tsx
+++ app/book_club/page.tsx
@@ -67,7 +67,7 @@ export default async function bookClub() {
       .select("*")
       .gt("end_date", new Date().toISOString())
       .lte("start_date", new Date().toISOString())
-      .limit(1)
+      .limit(0)
 
   const currentBook = data ? (data[0] ?? null) : null
 
